368e986b949449fe22c4c444c9b56cb718deec8b
dfralan <80047722+dfralan@users.noreply.github.com>
2021-09-22 19:07:48 -0300

    Update wordsToArray.py

--- wordsToArray.py
+++ wordsToArray.py
@@ -1,4 +1,4 @@
-cadena = "Hello incredulous person, maybe the answer was always there, but your ego did not let you see it, but dont get mad with your ego, his your teacher. Use this wisely, or do you really think that if you always had the truth, you would value it? Look at the sun and talk to him, it will give you even more answers."
+cadena = "Hello incredulous person, maybe the answer was always there, but your ego did not let you see it, but dont get mad with your ego, his your teacher. Use this wisely, or do you really think that if you always had the truth, you would value it. Look at the sun and talk to him, it will give you even more answers."
 
 w = cadena.replace("?","")
 x = w.replace(".","")
